7dc6c8544f2785ff56ba0a7a24672416b4db4803
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-07-28 23:24:40 +0530

    added text

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import mycomponent from './components/firstcomponent';
 
 const App=()=> {
   return (
     <View style={styles.container}>
-      <Text>Testing the App</Text>
+      <Text style={textStyle.container}>Testing the App</Text>
       <StatusBar style="auto" />
+      <Text>mycomponent</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+
+const textStyle = StyleSheet.create({
+  container:{
+    color: 'white'
   },
 });
 
