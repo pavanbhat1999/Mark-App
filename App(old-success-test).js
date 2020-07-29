4e2a5ffe9a64317ce4f3601c8ecbe56ee4f3df08
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-07-29 22:58:15 +0530

    Intro added

---
+++ App(old-success-test).js
@@ -0,0 +1,57 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert, ScrollView, SafeAreaView, SafeAreaViewComponent, TextInput,Image } from 'react-native';
+import Mycomponent from './components/Firstcomponent';
+
+const App = () => {
+  return (
+  
+    <ScrollView style={styles.container}>
+      
+      <Text style={styles.textStyle}>
+        This is the text displayed from the main App.js
+      </Text>
+      
+      <TextInput 
+        style={{backgroundColor:'white',height:50,width: 350,fontSize:50}} 
+        placeholder="Enter your text" />
+          
+      <StatusBar style="dark" />
+
+      <Button title="My button" onPress = {() => Alert.alert('button pressed')} />
+
+      <Mycomponent />
+      <Mycomponent />
+      <Mycomponent />
+      <Mycomponent />
+      <Mycomponent />
+      <Image
+          source={require('./images/solarized.jpg')}
+          style={{ width: 200, height: 200 }}
+        />
+      
+    </ScrollView>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+    //flex: 1,
+    backgroundColor: 'grey',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    position: 'relative',
+    top: 30,
+    
+  },
+  textStyle:{
+    color: 'white',
+    height: 100,
+    
+  },
+});
+
+
+export default App;
--- App.js
+++ App.js
@@ -1,59 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert, ScrollView, SafeAreaView, SafeAreaViewComponent, TextInput,Image } from 'react-native';
-import Mycomponent from './components/Firstcomponent';
+import { View,Text, Image, ImageBackground } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const App = () => {
-  return (
-  
-    <ScrollView style={styles.container}>
-      
-      <Text style={styles.textStyle}>
-        This is the text displayed from the main App.js
-      </Text>
-      
-      <TextInput 
-      style={{backgroundColor:'white',height:50,width: 350,fontSize:50}} 
-      placeholder="Enter your text" />
-          
-      <StatusBar style="dark" />
-
-      <Button title="My button" onPress = {() => Alert.alert('button pressed')} />
-      <Mycomponent />
-      <Mycomponent />
-      <Mycomponent />
-      <Mycomponent />
-      <Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent /><Mycomponent />
-
-      <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-      
-    </ScrollView>
+    return(
+<View style={{backgroundColor:'grey',flex:1,flexDirection:'column'}}>
     
-  );
+    <ImageBackground style={{flex:1,resizeMode:'cover',justifyContent:'center'}} source={require('./images/L.png') } />
+</View>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    
-    //flex: 1,
-    backgroundColor: 'grey',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    position: 'relative',
-    top: 30,
-    
-  },
-  textStyle:{
-    color: 'white',
-    height: 100,
-    
-  },
-});
-
 
 export default App;
