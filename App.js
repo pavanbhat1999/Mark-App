18329dfb08bf37d8ffb725e916f289729ae6b85e
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-07-29 16:18:23 +0530

    testing

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, ScrollView, SafeAreaView, SafeAreaViewComponent, TextInput } from 'react-native';
 import mycomponent from './components/firstcomponent';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={textStyle.container}>Testing the App</Text>
+      <Text style={textStyle.container}>
+        This is the text
+        
+      </Text>
+      <TextInput style={{backgroundColor:'white',height:50,width: 350,fontSize:50}} placeholder="Enter your text">
+          
+      </TextInput>
       <StatusBar style="auto" />
       <Button title="My button" onPress = {() => Alert.alert('button pressed')} />
       <Text>mycomponent</Text>
