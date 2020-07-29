45e987f10c04b2ef090f654344589696d03a7408
pavanbhat1999 <pavan.17.becs@acharya.ac.in>
2020-07-29 22:21:46 +0530

    TEST9

--- App.js
+++ App.js
@@ -5,16 +5,19 @@ import Mycomponent from './components/Firstcomponent';
 
 const App = () => {
   return (
+  
     <ScrollView style={styles.container}>
-      <View>
-      <Text style={textStyle.container}>
-        This is the text
-        
+      
+      <Text style={styles.textStyle}>
+        This is the text displayed from the main App.js
       </Text>
-      <TextInput style={{backgroundColor:'white',height:50,width: 350,fontSize:50}} placeholder="Enter your text">
+      
+      <TextInput 
+      style={{backgroundColor:'white',height:50,width: 350,fontSize:50}} 
+      placeholder="Enter your text" />
           
-      </TextInput>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
+
       <Button title="My button" onPress = {() => Alert.alert('button pressed')} />
       <Mycomponent />
       <Mycomponent />
@@ -28,29 +31,29 @@ const App = () => {
           }}
           style={{ width: 200, height: 200 }}
         />
-      </View>
+      
     </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     
-    // flex: 1,
+    //flex: 1,
     backgroundColor: 'grey',
     // alignItems: 'center',
     // justifyContent: 'center',
+    position: 'relative',
+    top: 30,
+    
+  },
+  textStyle:{
+    color: 'white',
+    height: 100,
     
   },
 });
 
-const textStyle = StyleSheet.create({
-  container:{
-    color: 'white',
-    height: 100,
-    position: 'relative',
-    top: 30,
-  },
-});
 
 export default App;
